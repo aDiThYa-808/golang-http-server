cc8d5fe66e99125c887b17962138409e7a4c1e75
aDiThYa-808 <adithyas824@gmail.com>
2025-10-12 19:58:45 +0530

    feat: add FormFile() to get and handle uploaded file

--- internal/handlers/upload.go
+++ internal/handlers/upload.go
@@ -6,5 +6,18 @@ import (
 )
 
 func UploadHandler(w http.ResponseWriter, r *http.Request){
-	log.Println("upload handler")
+	file,header,err := r.FormFile("file")
+
+	if err != nil{
+		log.Printf("Error: %v\n",err)
+		http.Error(w,"Failed to get file",400)
+		return
+	}
+
+	defer file.Close()
+
+	log.Println("Recieved file")
+	log.Printf("File name: %s\n",header.Filename)
+	log.Printf("File Size: %d bytes\n",header.Size)
+
 }
