61abb6cb61d049649f3054a0cb692f58cb7919b3
aDiThYa-808 <adithyas824@gmail.com>
2025-10-22 19:11:25 +0530

    chore: removed unnesasary log message

--- internal/middlewares/maxBodySize.go
+++ internal/middlewares/maxBodySize.go
@@ -8,7 +8,6 @@ import (
 func MaxBodySize(maxBytes int64) func(http.Handler) http.Handler{
 	return func(next http.Handler) http.Handler{
 		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request){
-			log.Println("MaxBodySize middleware")
 			r.Body = http.MaxBytesReader(w,r.Body,maxBytes)
 			next.ServeHTTP(w,r)
 		})
