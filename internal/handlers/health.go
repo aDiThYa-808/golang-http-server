a1b6888f427fb55e1dd23b73079f916820c0f0e9
aDiThYa-808 <adithyas824@gmail.com>
2025-10-22 19:22:51 +0530

    fix: renamed the handler method

--- internal/handlers/health.go
+++ internal/handlers/health.go
@@ -2,7 +2,7 @@ package handlers
 
 import "net/http"
 
-func HomeHandler(w http.ResponseWriter, r *http.Request) {
+func HealthHandler(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("OK\n"))
 }
 
